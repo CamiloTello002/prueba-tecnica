b317dfd47ab5ca4421080f97d6758b8305e4272a
CamiloTello002 <camilojtello5@gmail.com>
2025-05-16 13:54:44 -0500

    refactor: retrieve api base url from env variable

--- prueba-tecnica-frontend/src/constants/api.ts
+++ prueba-tecnica-frontend/src/constants/api.ts
@@ -1,4 +1,5 @@
-export const API_BASE_URL = 'http://localhost:3000/api';
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+console.log({ API_BASE_URL })
 export const AUTH_ENDPOINTS = {
   LOGIN: `${API_BASE_URL}/auth/login`,
   SIGNUP: `${API_BASE_URL}/user/register`,
